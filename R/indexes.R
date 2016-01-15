445398f1224d3acb9d710a7724d681918bb7d39b
Flavio Lozano <Flavjacj@gmail.com>
2016-01-14 19:41:49 -0500

    5/11 functions

--- R/indexes.R
+++ R/indexes.R
@@ -19,10 +19,55 @@ GRS <- function(values){
 #' @return Percentage of seed germinated
 #' @export
 
-GSP <- function(seeds, values){
+GRP <- function(seeds, values){
   sum <- sum(values,na.rm = TRUE)
   per <- (sum/seeds)*100
   per
 }
+
+#' ArcSin of Germination Values
+#' @description This function Calculate the ArcSin of Germination Values for normalization
+#' @param seeds Total seed sown
+#' @param values Germination values
+#' @author Flavio Lozano
+#' @return ArcSin of Germination Values
+#' @export
+
+ASG <- function(seeds, values){
+  sum <- sum(values,na.rm = TRUE)
+  per <- (sum/seeds)
+  rst <- asin(sqrt(per))
+  rst
+}
+
+#' Mean Germination Time
+#' @description This function Calculate the ArcSin of Germination Values for normalization
+#' @param values Germination values
+#' @author Flavio Lozano
+#' @return ArcSin of Germination Values
+#' @export
+
+MGT <- function(values){
+  sum <- sum(values,na.rm = TRUE)
+  days <- 0:length(values)
+  rst <- sum(days*values, na.rm = T)/sum
+  rst
+}
+
+
+#' Mean Germination Rate
+#' @description This function Calculate the Mean Germination Rate
+#' @param values Germination values
+#' @author Flavio Lozano
+#' @return Mean Germination Rate
+#' @export
+
+MGR <- function(values){
+  sum <- sum(values,na.rm = TRUE)
+  days <- 0:length(values)
+  rst <- sum/sum(days*values, na.rm = T)
+  rst
+}
+
 
 
