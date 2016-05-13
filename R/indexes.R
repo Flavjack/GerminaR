980d5bb0b6f7e9e25a82eccbecc17072c0ac463d
Flavio Lozano <Flavjacj@gmail.com>
2016-05-13 20:25:35 -0300

    General modifications

--- R/indexes.R
+++ R/indexes.R
@@ -1,14 +1,22 @@
-#' Germination Seed Number
-#' @description This function Calculate the Number of Germinated Seeds
-#' @param eval_days Germination evaluation days
-#' @author Flavio Lozano
-#' @return Number of seed germinated
-#' @export
+library(gsheet)
+library(agricolae)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
 
+url <- "https://docs.google.com/spreadsheets/d/1G9555qEa1TJ5NDJV4zGAffY-YZTJTE0keDpiANnJpIo/edit#gid=0"
+
+fb<- gsheet2tbl(url) %>% select(RPT:SDN, D01:D25)
+str(fb)
+
+#' Germinated Seed Number
+#' @description This function Calculate the Germination percentage
+#' @param seeds Total seed sown
+#' @param eval_days Germination evaluation days
+#' @return Number of seed germianated in the experimen for EU
+#' @export
 GRS <- function(eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  #ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
   ger
 }
 
@@ -16,186 +24,157 @@ GRS <- function(eval_days){
 #' @description This function Calculate the Germination percentage
 #' @param seeds Total seed sown
 #' @param eval_days Germination evaluation days
-#' @author Flavio Lozano
 #' @return Percentage of seed germinated
 #' @export
 
 GRP <- function(seeds, eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  #ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
   per <- (ger/seeds)*100
-  
   per
 }
+
 
 #' ArcSin of Germination eval_days
 #' @description This function Calculate the ArcSin of Germination eval_days for normalization
 #' @param seeds Total seed sown
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
-#' @return ArcSin of Germination eval_days
+#' @param eval_days Germination evalaluation days
+#' @return ArcSin of Germination percentage
 #' @export
 
 ASG <- function(seeds, eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  #ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
   per <-  (ger/seeds)
   rst <-  asin(sqrt(per))
   rst
 }
 
+
 #' Mean Germination Time
 #' @description This function Calculate the Mean Germination Time
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Mean Germination Time
 #' @export
 
 MGT <- function(eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  #ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
-  #days <- 0:length(eval_days)
-  ndays <- ncol(eval_days)-1
-  days <- 0:ndays
-  temp <- suppressWarnings(t(t(eval_days) * days)) #The transpose is due to multiple each component vector to column in DF
-  #rst <- apply(cbind(temp),1, sum, na.rm = T)/ger
-  rst <- apply(cbind(temp),1, sbsum)/ger
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
+  days <- 1:ncol(eval_days)
+  temp <- t(t(eval_days) * days)
+  rst <- apply(cbind(temp),1, sum, na.rm = T)/ger
   rst
 }
 
+
 #' Coefficient of Velocity of Germination
 #' @description This function Calculate the Coefficient of Velocity of Germination
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozanoa
+#' @param eval_days Germination evalaluation days
 #' @return Coefficient of Velocity of Germination
 #' @export
 
 CVG <- function(eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
-  #days <- 0:length(eval_days)
-  ndays <- ncol(eval_days)-1
-  days <- 0:ndays
-  temp <- suppressWarnings(t(t(eval_days) * days))
-  #mgt <- sum(days*eval_days, na.rm = T)/ger
-  mgt <- apply(cbind(temp),1, sbsum)/ger
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
+  days <- 1:ncol(eval_days)
+  temp <- t(t(eval_days) * days)
+  mgt <- apply(cbind(temp),1, sum, na.rm = TRUE)/ger
   rst <- (1/mgt)*100
   rst
 }
 
+
 #' Mean Germination Rate
 #' @description This function Calculate the Mean Germination Rate
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Mean Germination Rate
 #' @export
 
 MGR <- function(eval_days){
-  
-  ger <-  apply(cbind(eval_days), 1, sbsum)
-  #days <- 0:length(eval_days)
-  ndays <- ncol(eval_days)-1
-  days <- 0:ndays
-  temp <- suppressWarnings(t(t(eval_days) * days))
-  rst <- ger/apply(cbind(temp),1, sbsum)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
+  days <- 1:ncol(eval_days)
+  temp <- t(t(eval_days) * days)
+  rst <- ger/apply(cbind(temp),1, sum, na.rm = TRUE)
   rst
 }
 
+
 #' Germination Uncertainty
 #' @description This function Calculate the Germination Uncertainty
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Germination Uncertainty (U)
 #' @export
 
 GRU <- function(eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
-  #rst <- sum(eval_days/ger * log2(eval_days/ger), na.rm = T) * -1
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
   prod <- eval_days/ger * log2(eval_days/ger)
-  #rst <-  sum(prod, na.rm = T) * -1
-  rst <- apply(prod, 1, sbsum) * -1
+  rst <- apply(prod, 1, sum, na.rm = TRUE) * -1
   rst
-  }
+}
 
 #' Germination Synchronization Index
 #' @description This function Calculate the Germination Synchronization Index
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Germination Synchronization Index (Z)
 #' @export
 
 GSI <- function(eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
   temp <- eval_days*(eval_days - 1)/2
-  #smp <- sum( eval_days*(eval_days - 1)/2 , na.rm = TRUE)
-  smp <- apply(temp ,1, sbsum)
+  smp <- apply(temp ,1, sum, na.rm = TRUE)
   rst <- smp / (ger*(ger-1)/2)
   rst
 }
 
+
 #' Variance of Germination Time
 #' @description This function Calculate the Variance of Germination Time
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Variance of Germination Time
 #' @export
 
 VGT <- function(eval_days){
-  #ger <- sum(eval_days,na.rm = TRUE)
-  ger <-  apply(cbind(eval_days), 1, sbsum)
-  #days <- 0:length(eval_days)
-  ndays <- ncol(eval_days)-1
-  days <- 0:ndays
-  #mgt <- sum(days*eval_days, na.rm = T)/ger
-  mgt <- MGT(eval_days = eval_days)
-  #temp <- eval_days * (days-mgt)^2
-  temp <- eval_days*(t(t(eval_days)-mgt))^2
-  #rsp <- sum(eval_days * (days-mgt)^2, na.rm = TRUE)
-  rsp <- apply(temp, 1, sbsum)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
+  mgt <- MGT(eval_days)
+  days <- 1:ncol(eval_days)
+  daym <- rep.row(days,nrow(eval_days))
+  temp <- eval_days * (daym-mgt)^2
+  rsp <- apply(temp, 1, sum, na.rm = TRUE)
   rst <- rsp/(ger-1)
   rst
 }
 
+
 #' Standard deviation of the Germination Time
 #' @description This function Calculate the Standard deviation of the Germination Time
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Standard deviation of the Germination Time
 #' @export
 
 SDG <- function(eval_days){
-  ger <- sum(eval_days,na.rm = TRUE)
-  days <- 0:length(eval_days)
-  mgt <- sum(days*eval_days, na.rm = T)/ger
-  rsp <- sum(eval_days * (days-mgt)^2, na.rm = TRUE)
-  vgt <- rsp/(ger-1)
-  rst <- sqrt(vgt)
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
+  mgt <- MGT(eval_days)
+  days <- 1:ncol(eval_days)
+  daym <- rep.row(days,nrow(eval_days))
+  temp <- eval_days * (daym-mgt)^2
+  rsp <- apply(temp, 1, sum, na.rm = TRUE)
+  rst <- sqrt(rsp/(ger-1))
   rst
 }
 
+
 #' Coefficient of Variation of the Mean Germination Time
 #' @description This function Calculate the Coefficient of Variation of the Mean Germination Time
-#' @param eval_days Germination eval_days
-#' @author Flavio Lozano
+#' @param eval_days Germination evalaluation days
 #' @return Coefficient of Variation of the Mean Germination Time
 #' @export
 
 CoVG <- function(eval_days){
-  ger <- sum(eval_days,na.rm = TRUE)
-  days <- 0:length(eval_days)
-  mgt <- sum(days*eval_days, na.rm = T)/ger
-  rsp <- sum(eval_days * (days-mgt)^2, na.rm = TRUE)
-  vgt <- rsp/(ger-1)
-  sdg <- sqrt(vgt)
-  rst <- (sdg/mgt)*100
-  rst
+  ger <-  apply(cbind(eval_days), 1, sum, na.rm = TRUE)
+  mgt <- MGT(eval_days)
+  days <- 1:ncol(eval_days)
+  daym <- rep.row(days,nrow(eval_days))
+  temp <- eval_days * (daym-mgt)^2
+  rsp <- apply(temp, 1, sum, na.rm = TRUE)
+  sdg <- sqrt(rsp/(ger-1))
+  rst <-  (sdg/mgt)*100
 }
-
 
 
 
--- R/utils.R
+++ R/utils.R
@@ -1,26 +1,10 @@
-#' Evaluation days row names
-#' @description This function Calculate the Number of Germinated Seeds
-#' @param fieldbook Data frame with the germination evaluation
-#' @author Omar Benites
-#' @return Names of the evaluation days
-#' @export
 
-get_eval_days <- function(fieldbook){
-  factors <- c("RPT", "GNT", "SALT", "SDN", "ASG", "CVG", "CoVG", "GRP", "GRS", "GRU", "GSI", "MGR", "MGT", "SDG", "VGT")
-  eval_days <- names(fieldbook)
-  eval_days <- names(fieldbook)[!is.element(eval_days,set = factors)]
-  eval_days
+#' Repeated Rows in a data matrix
+#' @description This function made a data table with the values of germination variables
+#' @param x Row sequance for the data matrix
+#' @param n Number of rows for the data matrix
+#' @return Data MAtrix with day of the germination
+#' @export
+rep.row<-function(x,n){
+  matrix(rep(x,each=n),nrow=n)
 }
-
-#'This function allows sum columns and avoid NA values at the same time
-#'
-#' @param x vector of measurements
-#' @return A vector contains sum of values that avoid NA's
-#' @keywords sum, NA's
-#' @author Omar Benites 
-#' @export
-#'
-sbsum <- function(x){
-  z <- x[!is.na(x)]; 
-  ifelse(length(z), sum(z), NA)
-} 
