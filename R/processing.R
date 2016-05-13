8a629fa233b7f1359fd3a035b52bf8e023d38b2b
Flavio Lozano <Flavjacj@gmail.com>
2016-05-13 20:25:35 -0300

    General modifications

--- R/processing.R
+++ R/processing.R
@@ -1,53 +1,24 @@
-#' Calculation of the variables data matrix
-#' @description This function Calculate the Number of Germinated Seeds
-#' @param fieldbook Data frame with the germination evaluation
-#' @author Omar Benites
-#' @return Names of the evaluation days
-#' @export
 
-germcalculate <- function(fieldbook){
-  
-  seeds <- fieldbook$SDN
-  names_eval_days <- GerminaR::get_eval_days(fieldbook = fieldbook)
-  eval_days <- fieldbook[names_eval_days]
-  
-  
-  if(!is.element("GRS",names(fieldbook))) {fieldbook$GRS <- NA}
-  fieldbook <- within(fieldbook,{  
-    GRS <- GerminaR::GRS(eval_days = eval_days) 
-  })
-  
-  if(!is.element("GRP",names(fieldbook))) {fieldbook$GRP <- NA}
-  fieldbook <- within(fieldbook,{  
-    GRP <- GerminaR::GRP(seeds = seeds, eval_days = eval_days) 
-  })
-  
-  if(!is.element("ASG",names(fieldbook))) {fieldbook$ASG <- NA}
-  fieldbook <- within(fieldbook,{  
-    ASG <- GerminaR::ASG(seeds = seeds,eval_days = eval_days) 
-  })
-  
-  
-  if(!is.element("MGT",names(fieldbook))) {fieldbook$MGT <- NA}
-  fieldbook <- within(fieldbook,{  
-    MGT <- GerminaR::MGT(eval_days = eval_days) 
-  })
-  
-  
-  if(!is.element("CVG",names(fieldbook))) {fieldbook$CVG <- NA}
-  fieldbook <- within(fieldbook,{  
-    CVG <- GerminaR::CVG(eval_days = eval_days) 
-  })
-  
-  
-  if(!is.element("MGR",names(fieldbook))) {fieldbook$MGR <- NA}
-  fieldbook <- within(fieldbook,{  
-   MGR  <- GerminaR::MGR(eval_days = eval_days) 
-  })  
-  
- 
-  return(fieldbook)
-#   CVG CoVG
-# GRP
-# GRS, GRU, GSI, MGR, MGT, SDG, VGT
+#' Summary of germination variables
+#' @description This function made a data table with the values of germination variables
+#' @param data Data with the germination avaliation
+#' @return Data frame with the values of germination variables
+#' @export
+Summary_Ger <- function(data){
+  eval_days <- select(data, starts_with("D"))
+  SDN <- data$SDN
+  sm <- data %>% select(-starts_with("D")) %>% 
+    mutate(
+      GRS = GRS(eval_days), 
+      GRP = GRP(SDN,eval_days),
+      ASG = ASG(SDN,eval_days),
+      MGT = MGT(eval_days),
+      CVG = CVG(eval_days),
+      MGR = MGR(eval_days),
+      GRU = GRU(eval_days),
+      GSI = GSI(eval_days),
+      VGT = VGT(eval_days),
+      SDG = SDG(eval_days),
+      CoVG = CoVG(eval_days)
+    ) 
 }
