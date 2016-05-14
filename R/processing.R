9b9329769a4f1a6be8998bb0f618d5e310deff8c
Flavio Lozano <Flavjacj@gmail.com>
2016-05-14 01:05:09 -0300

    Rename

--- R/processing.R
+++ R/processing.R
@@ -4,21 +4,21 @@
 #' @param data Data with the germination avaliation
 #' @return Data frame with the values of germination variables
 #' @export
-Summary_Ger <- function(data){
+ger_summary <- function(data){
   eval_days <- select(data, starts_with("D"))
   SDN <- data$SDN
   sm <- data %>% select(-starts_with("D")) %>% 
     mutate(
-      GRS = GRS(eval_days), 
-      GRP = GRP(SDN,eval_days),
-      ASG = ASG(SDN,eval_days),
-      MGT = MGT(eval_days),
-      CVG = CVG(eval_days),
-      MGR = MGR(eval_days),
-      GRU = GRU(eval_days),
-      GSI = GSI(eval_days),
-      VGT = VGT(eval_days),
-      SDG = SDG(eval_days),
-      CoVG = CoVG(eval_days)
+      GRS = ger_GRS(eval_days), 
+      GRP = ger_GRP(SDN,eval_days),
+      ASG = ger_ASG(SDN,eval_days),
+      MGT = ger_MGT(eval_days),
+      CVL = ger_CVL(eval_days),
+      MGR = ger_MGR(eval_days),
+      GRU = ger_GRU(eval_days),
+      GSI = ger_GSI(eval_days),
+      VGT = ger_VGT(eval_days),
+      SDG = ger_SDG(eval_days),
+      CVG = ger_CVG(eval_days)
     ) 
 }
