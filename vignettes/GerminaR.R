f11e4d2798b2bde9cb0a322ee51c917adb2bc2f8
Omar Benites Alfaro <Omar Benites Alfaro>
2016-09-12 22:26:49 -0500

    Create the vignette with examples

--- vignettes/GerminaR.R
+++ vignettes/GerminaR.R
@@ -1,7 +1,7 @@
 ## ---- fig.show='hold'----------------------------------------------------
 plot(1:10)
 plot(10:1)
-
+#Create the vignette with examples
 ## ---- echo=FALSE, results='asis'-----------------------------------------
 knitr::kable(head(mtcars, 10))
 
