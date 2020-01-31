98ee44ed62075b2c9655a7f64c234fc57b613b04
Flavjack <flavjack@gmail.com>
2020-01-31 14:04:09 +0100

    icon code

---
+++ inst/GerminaQuant/www/icon.r
@@ -0,0 +1,18 @@
+library(hexSticker)
+
+img <- "inst/GerminaQuant/www/germination.png"
+exp <- "inst/GerminaQuant/www/icon.png"
+
+sticker(
+  img,
+  package="GerminaR", 
+  p_size=0, 
+  h_fill = "#b0d744",
+  s_x=0.99,
+  s_y=0.955,
+  s_width=1,
+  s_height = 1,
+  h_color = "#3D6C43",
+  filename= exp
+  )
+
