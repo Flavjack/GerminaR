a83d3ad3bcd5e312b5c88fb9cf79bc77b269390a
Flavjack <flavjack@gmail.com>
2020-11-17 23:15:10 +0100

    updt dep

--- inst/GerminaQuant/setup.R
+++ inst/GerminaQuant/setup.R
@@ -36,7 +36,7 @@ for (pkg in cran) {
 
 for (pkg in git) { 
   if( !require(sub(".*/", "", pkg), character.only = TRUE) ) {
-    devtools::install_github(pkg)
+    devtools::install_github(pkg, upgrade = TRUE)
     library(sub(".*/", "", pkg), character.only = TRUE)
   } 
 }  
