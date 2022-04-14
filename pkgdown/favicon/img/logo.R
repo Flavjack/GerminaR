25c62d9cc8f17d5c91236e4a6e8fc6006d149814
Flavio <flavjack@gmail.com>
2022-04-14 09:44:38 -0500

    updt logo script

--- pkgdown/favicon/img/logo.R
+++ pkgdown/favicon/img/logo.R
@@ -4,7 +4,9 @@
 
 library(huito)
 
-huito_fonts(c("Paytone One", "Lobster"))
+font <- c("Paytone One", "Permanent Marker")
+
+huito_fonts(font)
 
 label <- label_layout(size = c(5.08, 5.08)
                       , border_color = NA
@@ -21,39 +23,40 @@ label <- label_layout(size = c(5.08, 5.08)
                 , border_color = NA
                 , margin = -0.8
                 , position = c(2.54, 2.54)
-                , panel_color = "white"
+                , panel_color = "blue"
                 , panel_size = 5.08
                 ) %>%
   include_text(value = "GerminaR"
-               , font = "Paytone One"
+               , font[1]
                , size = 23
                , position = c(2.54, 3.56)
                , color = "#a64d79"
                ) %>%
   include_text(value = "inkaverse.com"
+               , font[2]
                , size = 6
                , position = c(3.9, 0.96)
                , angle = 30
                , color = "white"
-               ) %>%
-  label_print(filename = "pkgdown/favicon/img/GerminaR"
+               )
+
+label %>% label_print()
+
+logo <- label %>%
+  label_print(filename = tempfile()
               , margin = 0
               , paper = c(5.5, 5.5)
-              , viewer = T
-              , smpres = 250
-              , mode = "c"
+              , mode = "complete"
               )
 
 # transparent -------------------------------------------------------------
 # -------------------------------------------------------------------------
 
-logo <- list.files("pkgdown/favicon/img"
-                   , full.names = T
-                   , pattern = "GerminaR.pdf") %>%
+sticker <- logo %>%
   image_read_pdf()  %>% 
   image_crop(geometry = "600x600+40") %>% 
   image_crop(geometry = "560x600-40") %>% 
-  image_transparent('white') %>% 
+  image_transparent('blue') %>% 
   image_write("man/figures/logo.png")
 
 # -------------------------------------------------------------------------
@@ -62,38 +65,39 @@ logo <- list.files("pkgdown/favicon/img"
 
 library(huito)
 
-huito_fonts(c("Paytone One", "Lobster"))
+font <- c("Lobster")
+
+huito_fonts(font)
 
 label <- label_layout(size = c(5.08, 5.08)
                       , border_color = NA
                       , border_width = 0.5
                       , background = "white"
-) %>% 
+                      ) %>% 
   include_image(value = "pkgdown/favicon/img/logo_germinaquant.png"
                 , size = c(4.3, 4.3)
                 , position = c(2.55, 2.18)
-  ) %>%
+                ) %>%
   include_text(value = "GerminaQuant"
                , font = "Lobster"
                , size = 24
                , position = c(2.53, 4.69)
                , color = "#a64d79"
-  ) %>% 
-  label_print(filename = "pkgdown/favicon/img/GerminaQuant"
+               ) 
+
+label %>% label_print()
+
+logo <- label %>%
+  label_print(filename = tempfile()
               , margin = 0
               , paper = c(5.5, 5.5)
-              , viewer = T
-              , smpres = 250
-              , mode = "s"
+              , mode = "complete"
               )
 
 # transparent -------------------------------------------------------------
 # -------------------------------------------------------------------------
 
-logo <- list.files("pkgdown/favicon/img"
-                   , full.names = T
-                   , pattern = "GerminaQuant.pdf") %>%
+sticker <- logo %>%
   image_read_pdf()  %>% 
   image_write("pkgdown/favicon/img/GerminaQuant.png")
-
 
